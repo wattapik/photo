f9ab91ff23b4e87ed7e50153374c8ccf902426e1
ted a. ⭕ <105297660+wattapik@users.noreply.github.com>
2023-03-01 18:26:00 +0000

    Add files via upload

---
+++ generate.py
@@ -0,0 +1,40 @@
+image_list = []
+
+import os
+
+
+
+with open('index.html', 'w', encoding="utf-8") as f:
+   f.write('''<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>TED A. ⭕ GALLERY</title>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/porygon.gif">
+    <meta name='robots' content='noindex,follow' />
+    <style>
+      body {
+        font-family: monospace;
+        background-color: black;
+      }
+
+      a {
+        color: rgb(255, 0, 0)
+      }
+
+      a:visited {
+        color: rgb(128, 0, 255)
+      }
+    </style>
+  </head>
+  <body>
+    <span style="color: rgb(255, 0, 0);">TED A. GALLERY</span> ⭕<span style="color: rgb(128, 0, 255);">△</span>🌊&nbsp;
+    <ul>''')
+   for filename in os.listdir('img'):
+      f.write(f'     <li><a href = "./img/{filename}">{filename}</a></li>\n') 
+   f.write('''    </ul>
+    🐍📡☀️🌿🩸⚡🛢️🏔️🧬🔨💥🪱
+  </body>
+</html>
+''')
